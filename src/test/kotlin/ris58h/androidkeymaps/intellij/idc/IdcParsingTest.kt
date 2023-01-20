cf3b7f6ecd570db4c69f732524f5952c87a133b0
Ilya Rodionov <ris58h@gmail.com>
2023-01-20 22:29:40 +0300

    IdcParsingTest

---
+++ src/test/kotlin/ris58h/androidkeymaps/intellij/idc/IdcParsingTest.kt
@@ -0,0 +1,11 @@
+package ris58h.androidkeymaps.intellij.idc
+
+import com.intellij.testFramework.ParsingTestCase
+
+class IdcParsingTest : ParsingTestCase("", "idc", true, IdcParserDefinition()) {
+    override fun getTestDataPath() = "src/test/testData/idc"
+
+    fun testSimple() {
+        doTest(true)
+    }
+}
---
+++ src/test/testData/idc/simple.idc
@@ -0,0 +1,4 @@
+# Comment line 1
+xxx = yyy
+# Comment line 2
+aaa = bbb
