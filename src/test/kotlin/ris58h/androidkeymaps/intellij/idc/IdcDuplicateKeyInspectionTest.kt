551370baed248a16948ca07b6e84e8e83e485862
Ilya Rodionov <ris58h@gmail.com>
2023-01-25 12:26:58 +0300

    IdcDuplicateKeyInspectionTest

---
+++ src/test/kotlin/ris58h/androidkeymaps/intellij/idc/IdcDuplicateKeyInspectionTest.kt
@@ -0,0 +1,18 @@
+package ris58h.androidkeymaps.intellij.idc
+
+import com.intellij.testFramework.EditorTestUtil
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+class IdcDuplicateKeyInspectionTest : BasePlatformTestCase() {
+    override fun getTestDataPath(): String = "src/test/testData/idc"
+    private fun doFileTest() {
+        val testName = getTestName(true)
+        myFixture.configureByFile("$testName.idc")
+        EditorTestUtil.checkEditorHighlighting(myFixture, "$testDataPath/$testName.highlights.txt", null)
+    }
+
+    fun testDuplicateKey() {
+        myFixture.enableInspections(IdcDuplicateKeyInspection::class.java)
+        doFileTest()
+    }
+}
