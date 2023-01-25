9f8ec8136bf961377f269dd9107ba7463aefaeab
Ilya Rodionov <ris58h@gmail.com>
2023-01-26 01:33:39 +0300

    Key Layout: fix strtol

--- src/main/kotlin/ris58h/androidkeymaps/intellij/kl/psi/CodeEntry.kt
+++ src/main/kotlin/ris58h/androidkeymaps/intellij/kl/psi/CodeEntry.kt
@@ -11,14 +11,18 @@ interface CodeEntry : PsiElement {
 }
 
 private fun strtol(str: String): Long? {
-    val radix = when {
-        str.startsWith("0x") || str.startsWith("0X") -> 16
-        str.startsWith("0") -> 8
-        else -> 10
-    }
-    return try {
-        str.toLong(radix)
-    } catch (e: NumberFormatException) {
-        null
+    return if (str.startsWith("0x") || str.startsWith("0X")) {
+        try {
+            str.substring(2).toLong(16)
+        } catch (e: NumberFormatException) {
+            null
+        }
+    } else {
+        try {
+            val radix = if (str.startsWith("0")) 8 else 10
+            str.toLong(radix)
+        } catch (e: NumberFormatException) {
+            null
+        }
     }
 }
