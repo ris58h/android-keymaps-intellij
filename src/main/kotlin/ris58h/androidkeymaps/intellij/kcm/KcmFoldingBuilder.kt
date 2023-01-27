4f8f3b50f77ae09dbed7d2227acf7f73de7152db
Ilya Rodionov <ris58h@gmail.com>
2023-01-27 20:21:29 +0300

    KcmFoldingBuilder

---
+++ src/main/kotlin/ris58h/androidkeymaps/intellij/kcm/KcmFoldingBuilder.kt
@@ -0,0 +1,21 @@
+package ris58h.androidkeymaps.intellij.kcm
+
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingBuilderEx
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.openapi.editor.Document
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import ris58h.androidkeymaps.intellij.kcm.psi.KcmKeyPropertiesBlock
+
+class KcmFoldingBuilder : FoldingBuilderEx() {
+    override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
+        return PsiTreeUtil.findChildrenOfType(root, KcmKeyPropertiesBlock::class.java)
+            .map { FoldingDescriptor(it.node, it.textRange, null, "{...}") }
+            .toTypedArray()
+    }
+
+    override fun getPlaceholderText(node: ASTNode): String? = null
+
+    override fun isCollapsedByDefault(node: ASTNode): Boolean = false
+}
