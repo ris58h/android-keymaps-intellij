0390bec6a07954fca3e6db737c362bf995ecacc5
Ilya Rodionov <ris58h@gmail.com>
2023-01-28 20:05:03 +0300

    Idc: check that a property is placed on a single line

--- src/main/kotlin/ris58h/androidkeymaps/intellij/idc/IdcAnnotator.kt
+++ src/main/kotlin/ris58h/androidkeymaps/intellij/idc/IdcAnnotator.kt
@@ -4,21 +4,31 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.psi.PsiElement
+import com.intellij.psi.TokenType
 import com.intellij.psi.util.elementType
+import ris58h.androidkeymaps.intellij.idc.psi.IdcProperty
 import ris58h.androidkeymaps.intellij.idc.psi.IdcTypes
 
 private val RESERVED_CHARS = setOf(' ', '\\', '\"')
 
 class IdcAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        if (element.elementType != IdcTypes.VALUE) {
-            return
+        if (element.elementType == IdcTypes.VALUE) {
+            val value = element.text
+            val reservedChars = reservedChars(value)
+            if (reservedChars.isNotEmpty()) {
+                holder.newAnnotation(HighlightSeverity.ERROR, errorMessage(reservedChars))
+                    .range(element)
+                    .create()
+            }
         }
-        val value = element.text
-        val reservedChars = reservedChars(value)
-        if (reservedChars.isNotEmpty()) {
-            holder.newAnnotation(HighlightSeverity.ERROR, errorMessage(reservedChars))
+
+        if (element.elementType == TokenType.WHITE_SPACE
+            && element.parent is IdcProperty
+            && element.text.contains('\n')) {
+            holder.newAnnotation(HighlightSeverity.ERROR, "Property must be placed on a single line")
                 .range(element)
+                .afterEndOfLine()
                 .create()
         }
     }
