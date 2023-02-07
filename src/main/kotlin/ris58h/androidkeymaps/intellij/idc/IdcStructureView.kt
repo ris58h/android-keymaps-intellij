99424f7cceb508f4d0fb1ffa3c0c3fc7ae9723e6
Ilya Rodionov <ris58h@gmail.com>
2023-02-07 20:34:35 +0300

    IdcStructureView

--- src/main/kotlin/ris58h/androidkeymaps/intellij/idc/IdcStructureView.kt
+++ src/main/kotlin/ris58h/androidkeymaps/intellij/idc/IdcStructureView.kt
@@ -1,14 +1,44 @@
 package ris58h.androidkeymaps.intellij.idc
 
 import com.intellij.ide.projectView.PresentationData
-import com.intellij.ide.structureView.StructureViewTreeElement
+import com.intellij.ide.structureView.*
 import com.intellij.ide.util.treeView.smartTree.TreeElement
+import com.intellij.lang.PsiStructureViewFactory
 import com.intellij.navigation.ItemPresentation
+import com.intellij.openapi.editor.Editor
 import com.intellij.psi.NavigatablePsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
 import ris58h.androidkeymaps.intellij.idc.psi.IdcFile
 import ris58h.androidkeymaps.intellij.idc.psi.IdcProperty
 import ris58h.androidkeymaps.intellij.idc.psi.impl.IdcPropertyImpl
+
+class IdcStructureViewFactory : PsiStructureViewFactory {
+    override fun getStructureViewBuilder(psiFile: PsiFile): StructureViewBuilder {
+        return object : TreeBasedStructureViewBuilder() {
+            override fun createStructureViewModel(editor: Editor?): StructureViewModel {
+                return IdcStructureViewModel(editor, psiFile)
+            }
+        }
+    }
+}
+
+class IdcStructureViewModel(editor: Editor?, psiFile: PsiFile) :
+    StructureViewModelBase(psiFile, editor, IdcStructureViewElement(psiFile)),
+    StructureViewModel.ElementInfoProvider {
+
+    override fun isAlwaysShowsPlus(element: StructureViewTreeElement?): Boolean {
+        return false
+    }
+
+    override fun isAlwaysLeaf(element: StructureViewTreeElement): Boolean {
+        return element.value is IdcProperty
+    }
+
+    override fun getSuitableClasses(): Array<Class<IdcProperty>> {
+        return arrayOf(IdcProperty::class.java)
+    }
+}
 
 class IdcStructureViewElement(private val myElement: NavigatablePsiElement) : StructureViewTreeElement {
     override fun getValue(): Any {
